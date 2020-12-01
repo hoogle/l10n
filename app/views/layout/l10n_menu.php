22c67c030636e39288ad85e1ba4a928f75c6a597
Hoogle Wang <hoogle.wang@gmail.com>
2020-12-01 11:47:56 +0800

    Remove left menu items

--- app/views/layout/l10n_menu.php
+++ app/views/layout/l10n_menu.php
@@ -13,23 +13,6 @@
                             </li>
 <?php endforeach ?>
 
-                        	<li class="text-muted menu-title">Compare</li>
-                            <li class="">
-                                <a href="/tool/compare/portal/en-US" class="waves-effect"><i class="ti-pencil-alt"></i><span>Portal en-US</span></a>
-                            </li>
-                            <li class="">
-                                <a href="/tool/compare/portal/ja-JP" class="waves-effect"><i class="ti-pencil-alt"></i><span>Portal ja-JP</span></a>
-                            </li>
-                            <li class="">
-                                <a href="/tool/compare/goface/en-US" class="waves-effect"><i class="ti-pencil-alt"></i><span>GoFace en-US</span></a>
-                            </li>
-                        	<li class="text-muted menu-title">Export</li>
-                            <li class="">
-                                <a href="/tool/export?p=api_error_code" class="waves-effect"><i class="ti-pencil-alt"></i><span>api_error_code</span></a>
-                            </li>
-                            <li class="">
-                                <a href="/tool/export?p=goface_portal" class="waves-effect"><i class="ti-pencil-alt"></i><span>Goface Portal</span></a>
-                            </li>
 
                         </ul>
                         <div class="clearfix"></div>
