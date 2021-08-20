fefb2d2ac2f72bfafbfd714a0d2bd0392c46bb04
Hoogle Wang <hoogle.wang@gmail.com>
2021-08-20 15:34:55 +0800

    Fix l10n domain

--- app/config/config.php
+++ app/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = (ENVIRONMENT=='production') ? 'https://l10n.goface.me' : 'http://localhost:8080';
+$config['base_url'] = (ENVIRONMENT=='production') ? 'https://l10n.astra.cloud' : 'http://localhost:8080';
 
 /*
 |--------------------------------------------------------------------------
