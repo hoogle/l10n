a3fb56a5ed9809b6d8f75f8c77f2c0ccb78b5046
Mei <mei@astra.cloud>
2021-08-25 14:49:13 +0800

    Fix email list dropmenu

--- app/views/email_list.php
+++ app/views/email_list.php
@@ -39,7 +39,7 @@
 <?php if ($email_data) : ?>
             <div class="row m-b-10" style="padding-bottom: 8px; font-weight: 600; vertical-align: bottom; border-bottom: 2px solid #ebeff2;">
                 <div class="d-flex emailLanRow">
-                    <div>Email category</div><div class="flex-fill">Subject</div><div>Last update time</div><div>Action</div>
+                    <div>Email category</div><div class="flex-fill">Subject</div><div style="flex: 0 1 190px;">Last update time</div><div style="flex: 0 1 190px;">Action</div>
                 </div>
             </div>
             <div id="emalLanList">
@@ -51,18 +51,20 @@
             <div class="flex-fill">
                 <?php echo $row["zh-TW"]; ?>
             </div>
-            <div><?php echo $row["last_updated_at"] ?></div>
-            <div>
-                <div class="dropdown">
-                    <button class="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown">Preview
-                    <span class="caret"></span></button>
-                    <ul class="dropdown-menu">
-<?php foreach ($user_langs as $lang) : ?>
-                        <li><a href="/email/preview?p=<?php echo $p; ?>&item=<?php echo $row["item"]; ?>&lang=<?php echo $lang; ?>"><?php echo $lang; ?></a></li>
-<?php endforeach ?>
-                    </ul>
+            <div style="flex: 0 1 190px;"><?php echo $row["last_updated_at"] ?></div>
+            <div class="d-flex" style="flex: 0 1 190px;">
+                <div class="btn-group dropdown">
+                    <button class="btn btn-success dropdown-toggle" type="button" aria-haspopup="true" aria-expanded="false" data-toggle="dropdown">
+                    <span class="fa fa-chevron-circle-left"></span>
+                    Preview
+                    </button>
+                        <ul class="dropdown-menu" style="top: -40px; left: -160px;">
+    <?php foreach ($user_langs as $lang) : ?>
+                            <li><a href="/email/preview?p=<?php echo $p; ?>&item=<?php echo $row["item"]; ?>&lang=<?php echo $lang; ?>"><?php echo $lang; ?></a></li>
+    <?php endforeach ?>
+                        </ul>
                 </div>
-                <button class="emailEditBtn btn btn-primary" data-id="<?php echo $row["item"]; ?>"><i class="ti-pencil"></i> Edit</button>
+                <button class="emailEditBtn btn btn-primary m-l-10" data-id="<?php echo $row["item"]; ?>"><i class="ti-pencil"></i> Edit</button>
             </div>
         </div>
     </div>
